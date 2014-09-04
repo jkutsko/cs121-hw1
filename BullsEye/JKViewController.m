75e83362ac0d89ae6ad30c02eef520773dcbc4c5
Guest User <Guest@peach.cs.hmc.edu>
2014-09-04 15:48:33 -0700

    Added slider placement detection when you press the main button

--- BullsEye/JKViewController.m
+++ BullsEye/JKViewController.m
@@ -13,6 +13,9 @@
 @end
 
 @implementation JKViewController
+{
+    int _currentValue;
+}
 
 - (void)viewDidLoad
 {
@@ -27,12 +30,21 @@
 }
 
 - (IBAction)showAlert {
+    
+    NSString *message =
+    [NSString stringWithFormat: @"The value of the slider is: %d", _currentValue];
+    
     UIAlertView *alertView = [[UIAlertView alloc]
     initWithTitle:@"Hello, World"
-    message:@"This is my first app!"
+    message:message
     delegate:nil
     cancelButtonTitle:@"Awesome"
     otherButtonTitles:nil];
     [alertView show];
 }
+
+- (IBAction)sliderMoved:(UISlider *)slider {
+    _currentValue = lroundf(slider.value);
+}
+
 @end
