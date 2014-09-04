163d3eadd02c94d8f83b7472a1de6e73dc98c618
Guest User <Guest@peach.cs.hmc.edu>
2014-09-04 14:48:11 -0700

    Initial Commit

---
+++ BullsEye/JKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JKAppDelegate.h
+//  BullsEye
+//
+//  Created by Guest User on 9/4/14.
+//  Copyright (c) 2014 Josh Kutsko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
