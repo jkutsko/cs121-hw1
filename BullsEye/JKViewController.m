e78b18e15f678ff9e381fc3bad28c7d6aa6e62a2
Guest User <Guest@plum.cs.hmc.edu>
2014-09-04 18:55:20 -0700

    Finished everything except starting over and adding information to the info button. Those buttons currently do nothing.

--- BullsEye/JKViewController.m
+++ BullsEye/JKViewController.m
@@ -16,6 +16,8 @@
 {
     int _currentValue;
     int _targetValue;
+    int _score;
+    int _round;
 }
 
 - (void)viewDidLoad
@@ -27,6 +29,7 @@
 }
 
 - (void)startNewRound {
+    _round += 1;
     _targetValue = 1 + arc4random_uniform(100);
     _currentValue = 50;
     self.slider.value = _currentValue;
@@ -40,16 +43,35 @@
 
 - (void)updateLabels {
     self.targetLabel.text = [NSString stringWithFormat:@"%d", _targetValue];
+    
+    self.scoreLabel.text = [NSString stringWithFormat:@"%d", _score];
+    
+    self.roundLabel.text = [NSString stringWithFormat:@"%d", _round];
+
 }
 
 - (IBAction)showAlert {
     
+    int difference = abs(_currentValue - _targetValue);
+    int points = 100 - difference;
+    _score += points;
+    
+    NSString *title;
+    if (difference == 0) { title = @"Perfect!";
+    } else if (difference < 5) {
+        title = @"You almost had it!";
+    } else if (difference < 10) {
+        title = @"Pretty good!";
+    } else {
+        title = @"Not even close...";
+    }
+    
     NSString *message = [NSString stringWithFormat:
-        @"The value of the slider is: %d\nThe target value is: %d",
-        _currentValue, _targetValue];
+        @"You scored %d points",
+         points];
     
     UIAlertView *alertView = [[UIAlertView alloc]
-    initWithTitle:@"Hello, World"
+    initWithTitle:title
     message:message
     delegate:nil
     cancelButtonTitle:@"Awesome"
