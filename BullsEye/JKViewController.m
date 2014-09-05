568971cb4f0740eb8cd8b30a9cfa5dc5baf1ee3c
Guest User <Guest@plum.cs.hmc.edu>
2014-09-04 18:38:45 -0700

    added updates for the target value label

--- BullsEye/JKViewController.m
+++ BullsEye/JKViewController.m
@@ -21,15 +21,25 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-    _currentValue = self.slider.value;
+    [self startNewRound];
+    [self updateLabels];
+	
+}
+
+- (void)startNewRound {
     _targetValue = 1 + arc4random_uniform(100);
+    _currentValue = 50;
+    self.slider.value = _currentValue;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)updateLabels {
+    self.targetLabel.text = [NSString stringWithFormat:@"%d", _targetValue];
 }
 
 - (IBAction)showAlert {
@@ -45,6 +55,8 @@
     cancelButtonTitle:@"Awesome"
     otherButtonTitles:nil];
     [alertView show];
+    [self startNewRound];
+    [self updateLabels];
 }
 
 - (IBAction)sliderMoved:(UISlider *)slider {
