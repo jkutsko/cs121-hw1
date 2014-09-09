0e593749fd8e542e2ba7d677e67a22d4e01ac537
Guest User <Guest@plum.cs.hmc.edu>
2014-09-08 22:29:22 -0700

    Made slider look nice as well.

--- BullsEye/JKViewController.m
+++ BullsEye/JKViewController.m
@@ -25,9 +25,21 @@
     [super viewDidLoad];
     [self startNewGame];
     [self updateLabels];
+    UIImage *thumbImageNormal = [UIImage imageNamed:@"SliderThumb-Normal"];
+    [self.slider setThumbImage:thumbImageNormal forState:UIControlStateNormal];
+    UIImage *thumbImageHighlighted = [UIImage imageNamed:@"SliderThumb-Highlighted"];
+    [self.slider setThumbImage:thumbImageHighlighted forState:UIControlStateHighlighted];
+    UIImage *trackLeftImage =
+    [[UIImage imageNamed:@"SliderTrackLeft"]
+     resizableImageWithCapInsets:UIEdgeInsetsMake(0, 14, 0, 14)];
+    [self.slider setMinimumTrackImage:trackLeftImage forState:UIControlStateNormal];
+    UIImage *trackRightImage =
+    [[UIImage imageNamed:@"SliderTrackRight"]
+     resizableImageWithCapInsets:UIEdgeInsetsMake(0, 14, 0, 14)];
+    [self.slider setMaximumTrackImage:trackRightImage forState:UIControlStateNormal];
 	
 }
-
+     
 - (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex
 {
     [self startNewRound];
