3ff83f6c7bff4b86201da327cdf263853496836d
Guest User <Guest@plum.cs.hmc.edu>
2014-09-08 21:48:57 -0700

    Added the information screen and made it functional

---
+++ BullsEye/AboutViewController.h
@@ -0,0 +1,15 @@
+//
+//  AboutViewController.h
+//  BullsEye
+//
+//  Created by Guest User on 9/8/14.
+//  Copyright (c) 2014 Josh Kutsko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+- (IBAction)close;
+
+@end
